b70da0704e397944841fc6b36c70971df989e7c6
Yerrappagari Bayanna <ybadri762@gmail.com>
2025-07-20 14:59:38 +0530

    second method

---
+++ git.js
@@ -0,0 +1,6 @@
+let btn=document.getElementById("ent");
+let b=document.getElementById("b");
+btn.addEventListener("click", function() {
+    b.style.color="red";
+    b.style.backgroundColor="black";
+})
